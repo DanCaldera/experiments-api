3f4f0259188c7b42c4662ec01941144e407ea4a1
Daniel Caldera <dan@wait3.com>
2022-10-25 13:53:45 -0500

    UPDATE: Added favicon.ico

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 import os
 
 app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
