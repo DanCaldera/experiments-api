d74d345090b728ffb7ac95efbc8daf1e6c9cedbd
Daniel Caldera <dan@wait3.com>
2022-11-09 00:33:55 -0600

    updated main.py

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
