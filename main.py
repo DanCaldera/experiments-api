8fa3b2ea0521c8d6f1f71e828357bbcc906d5e20
Daniel Caldera <dan@wait3.com>
2022-11-10 03:27:23 -0600

    min

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from flask import Flask, send_from_directory, jsonify
 import os
-from flaskr import create_app
+
+app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route("/")
+def index():
+    # return render_template("index.html")
+    return jsonify({"api": "ok"})
+
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, port=os.getenv("PORT", default=5000))
