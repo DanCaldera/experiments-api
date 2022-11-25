4d8384f58e5aa0f9bd00f5e63bbf64e93bf1b7c2
Daniel Caldera <dan@wait3.com>
2022-11-25 10:35:55 -0600

    Added transcribe

---
+++ flaskr/transcribe.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+import pytube
+import whisper
+
+bp = Blueprint('transcribe', __name__)
+
+
+@bp.route('/transcribe')
+def transcribe():
+    url = "https://www.youtube.com/watch?v=UuTcTAu__pY"
+    model = whisper.load_model("tiny")
+    youtube_vid = pytube.YouTube(url)
+    youtube_vid.streams.get_audio_only().download(filename='audio.mp4')
+    result = model.transcribe('audio.mp4')
+    return result['text']
